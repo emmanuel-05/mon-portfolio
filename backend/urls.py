6d8acced2513749d233b3e5ed630d4b9ea262a73
emmanuel-05 <gnamien.em@gmail.com>
2026-06-16 02:38:32 +0000

    admin user creation

--- backend/urls.py
+++ backend/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/projets/', include('projets.urls')),
 ]
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
